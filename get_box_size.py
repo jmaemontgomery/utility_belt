3574d615ca1c49d6dff33cf5e2d40cb7dabea953
Julia Montgomery <juliamontgomery@vt.edu>
2023-06-09 14:33:11 -0500

    added option to change target phrase to replace

--- get_box_size.py
+++ get_box_size.py
@@ -3,17 +3,18 @@
 # Assumes target to be styled "<BOXX>", "<BOXY>", and "<BOXZ>"
 import argparse
 
-# To do: Edit to include user flags for string to replace with box size
+parser = argparse.ArgumentParser(description='Scrape a box size from an OpenMM-Style state file (in nm) & replace it in a target file (in angstrom). Assumes your target file is styled "<BOXX>", "<BOXY>", and "<BOXZ>" where you want it replaced, unless user defines differently')
 
-parser = argparse.ArgumentParser(description='Scrape a box size from an OpenMM-Style state file (in nm) & replace it in a target file (in angstrom). Assumes your target file is styled "<BOXX>", "<BOXY>", and "<BOXZ>" where you want it replaced.')
-
-parser.add_argument('-rst', type = str, help = 'Path to your RST file.')
-parser.add_argument('-target', type = str, help = 'Path to your target file.')
+parser.add_argument('-rstFile', type = str, required = True, help = 'Path to your RST file.')
+parser.add_argument('-targetFile', type = str, required = True, help = 'Path to your target file.')
+parser.add_argument('-replaceX', type = str, default = "<BOXX>", help = 'Change target phrase (default "<BOXX>")')
+parser.add_argument('-replaceY', type = str, default = "<BOXY>", help = 'Change target phrase (default "<BOXY>")')
+parser.add_argument('-replaceZ', type = str, default = "<BOXZ>", help = 'Change target phrase (default "<BOXZ>")')
 
 args = parser.parse_args()
 
-rstfile = args.rst
-targetfile = args.target
+rstfile = args.rstFile
+targetfile = args.targetFile
 
 f = open(rstfile, 'r')
 
@@ -26,13 +27,13 @@ while True:
 
     if line.split()[0] == "<A":
         size = line.split()[1].strip('x="')
-        box['A'] = str(float(size)*10)
+        box['X'] = str(float(size)*10)
     elif line.split()[0] == "<B":
         size = line.split()[2].strip('y="')
-        box['B'] = str(float(size)*10)
+        box['Y'] = str(float(size)*10)
     elif line.split()[0] == "<C":
         size = line.split()[3].strip('z="').strip('"/>')
-        box['C'] = str(float(size)*10)
+        box['Z'] = str(float(size)*10)
     else:
         pass
 
@@ -44,9 +45,9 @@ with open(targetfile, 'r') as file :
     filedata = file.read()
 
 # Replace the target string, in this case box vectors
-filedata = filedata.replace('<BOXX>', box.get('A'))
-filedata = filedata.replace('<BOXY>', box.get('B'))
-filedata = filedata.replace('<BOXZ>', box.get('C'))
+filedata = filedata.replace(args.replaceX, box.get('X'))
+filedata = filedata.replace(args.replaceY, box.get('Y'))
+filedata = filedata.replace(args.replaceZ, box.get('Z'))
 
 # Write the new edited file
 with open(targetfile, 'w') as file:
